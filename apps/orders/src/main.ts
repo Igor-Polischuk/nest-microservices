02a7c4f71b7e6ea4c9dcc8f1b2a487d4f889dbab
IgorPolischuk <igor.polischuk@sysgears.com>
2024-05-21 12:02:08 +0300

    removed unused apps

--- apps/orders/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { OrdersModule } from './orders.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(OrdersModule);
-  await app.listen(3000);
-}
-bootstrap();
--- apps/products/src/products.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class ProductsService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-}
